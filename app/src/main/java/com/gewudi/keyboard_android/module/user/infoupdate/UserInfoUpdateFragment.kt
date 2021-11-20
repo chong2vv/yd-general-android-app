c51e400752836f25e777cde911fb24e7c758ff22
wangyuandong <chong2vv@163.com>
2021-11-20 16:47:45 +0800

    新增相机

--- app/src/main/java/com/gewudi/keyboard_android/module/user/infoupdate/UserInfoUpdateFragment.kt
+++ app/src/main/java/com/gewudi/keyboard_android/module/user/infoupdate/UserInfoUpdateFragment.kt
@@ -1,7 +1,10 @@
 package com.gewudi.keyboard_android.module.user.infoupdate
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.gewudi.keyboard_android.base.BaseFragment
@@ -12,6 +15,22 @@ import com.gewudi.keyboard_android.constant.PageName
 import com.gewudi.keyboard_android.constant.UserUpdateType
 import com.gewudi.keyboard_android.databinding.FragmentUserUpdateBinding
 import com.gewudi.keyboard_android.module.login.UserRegisterFragment
+import com.gewudi.keyboard_android.util.GlideEngine
+import com.luck.picture.lib.PictureSelector
+import com.luck.picture.lib.config.PictureConfig
+import com.luck.picture.lib.config.PictureMimeType
+import com.lxj.xpopup.interfaces.OnSelectListener
+
+import com.lxj.xpopup.XPopup
+import com.luck.picture.lib.entity.LocalMedia
+
+import com.luck.picture.lib.listener.OnResultCallbackListener
+
+
+
+
+
+
 
 class UserInfoUpdateFragment : BaseFragment<FragmentUserUpdateBinding>(
     FragmentUserUpdateBinding::inflate) {
@@ -25,6 +44,30 @@ class UserInfoUpdateFragment : BaseFragment<FragmentUserUpdateBinding>(
 
     private fun initView() {
         val fragmentId = this.id
+
+        viewBinding.userFaceCard.setOnClickListener {
+            XPopup.Builder(context)
+                .isDarkTheme(true)
+                .hasShadowBg(true)
+                .isViewMode(true) //                        .enableDrag(false)
+                //                            .hasBlurBg(true)
+                //                            .isDestroyOnDismiss(true) //对于只使用一次的弹窗，推荐设置这个
+                .asBottomList(
+                    "选择图片方式:", arrayOf("手机拍照", "相册选择")
+                ) { position, text ->
+                    run {
+                        Toast.makeText(context, "点击了 ===== "+position, Toast.LENGTH_SHORT).show()
+                        when(position){
+                            0 -> chooseImageForCamera()
+                            1 -> chooseImageForPhotoAlbum()
+                            else -> {
+
+                            }
+                        }
+                    }
+                }.show()
+        }
+
         viewBinding.rvList.init(
             XRecyclerView.Config()
                 .setPullRefreshEnable(false)
@@ -71,6 +114,38 @@ class UserInfoUpdateFragment : BaseFragment<FragmentUserUpdateBinding>(
                 })
         )
 
+    }
+
+    fun chooseImageForCamera() {
+        PictureSelector.create(this)
+            .openCamera(PictureMimeType.ofImage())
+            .imageEngine(GlideEngine.createGlideEngine())
+            .forResult(object : OnResultCallbackListener<LocalMedia?> {
+                override fun onResult(result: List<LocalMedia?>) {
+                    // 结果回调
+                }
+
+                override fun onCancel() {
+                    // 取消
+                }
+            })
+    }
+
+    fun chooseImageForPhotoAlbum() {
+        PictureSelector.create(this)
+            .openGallery(PictureMimeType.ofImage())
+            .imageEngine(GlideEngine.createGlideEngine()) // 请参考Demo GlideEngine.java
+            .forResult(object : OnResultCallbackListener<LocalMedia?> {
+                override fun onResult(result: List<LocalMedia?>) {
+                    // 结果回调
+                    print("回调啦~~~~")
+                }
+
+                override fun onCancel() {
+                    // 取消
+                    print("回调啦~~~~")
+                }
+            })
     }
 
     @PageName
